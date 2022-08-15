f7588696764e31f14b54d3e57e3813597dd795b1
Herminio Neto <herminio_neto@outlook.com>
2022-08-15 10:24:04 -0300

    Criado condicional de vencedor no css

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -20,6 +20,7 @@ function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
   const [currentPlayer, setCurrentPlayer] = useState(-1)
   const [winner, setWinner] = useState(0)
+  const [winnerLine, setWinnerLine] = useState([])
 
   const handleClick = (pos) => {
     if (gameState[pos] === 0 && winner === 0) {
@@ -35,7 +36,7 @@ function Game () {
       const sum = values.reduce((acc, value) => acc + value)
       if (sum === 3 || sum === -3) {
         setWinner(sum / 3)
-        // console.log('Winner: ', sum/3)
+        setWinnerLine(line)
       }
     })
   }
@@ -43,6 +44,11 @@ function Game () {
   const handleReset = () => {
     setGameState(Array(9).fill(0))
     setWinner(0)
+    setWinnerLine([])
+  }
+
+  const verifyWinnerLine = (pos) => {
+    return winnerLine.find((value) => value === pos) !== undefined
   }
 
   // O useEffect sempre terá dois parâmetros. O primeiro é uma função
@@ -62,6 +68,7 @@ function Game () {
               key={`game-option-pos-${pos}`}s
               status={value}
               onClick={() => handleClick(pos)}
+              isWinner={verifyWinnerLine(pos)}
             />
           )
         }
