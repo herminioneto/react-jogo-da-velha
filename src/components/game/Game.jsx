99c65e03e56296ef2bc0119e6803566231490ae0
Herminio Neto <herminio_neto@outlook.com>
2022-08-25 10:13:06 -0300

    Criado visual placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
 import GameInfo from '../gameinfo/GameInfo'
+import Score from '../score/Score'
 
 const winnerTable = [
   [0, 1, 2],
@@ -73,27 +74,30 @@ function Game () {
   }, [winner])
 
   return (
-    <div className={styles.gameContent}>
-      <div className={styles.game}>
-        {
-          gameState.map((value, pos) =>
-            <GameOption 
-              key={`game-option-pos-${pos}`}s
-              status={value}
-              onClick={() => handleClick(pos)}
-              isWinner={verifyWinnerLine(pos)}
-              isDraw={draw}
-            />
-          )
-        }
+    <>
+      <div className={styles.gameContent}>
+        <div className={styles.game}>
+          {
+            gameState.map((value, pos) =>
+              <GameOption 
+                key={`game-option-pos-${pos}`}s
+                status={value}
+                onClick={() => handleClick(pos)}
+                isWinner={verifyWinnerLine(pos)}
+                isDraw={draw}
+              />
+            )
+          }
+        </div>
+        <GameInfo
+          currentPlayer={currentPlayer}
+          winner={winner}
+          onReset={handleReset}
+          isDraw={draw}
+        />
       </div>
-      <GameInfo
-        currentPlayer={currentPlayer}
-        winner={winner}
-        onReset={handleReset}
-        isDraw={draw}
-      />
-    </div>
+      <Score />
+    </>
   )
 }
 
